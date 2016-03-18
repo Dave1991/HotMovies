8b72e2ab7147e237a25a4df944f32d04e7b6f3fc
dave <xanxusiou@163.com>
2016-03-18 22:18:26 +0800

    Initial Commit

---
+++ HotMovies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HotMovies
+//
+//  Created by DaveLam on 16/3/18.
+//  Copyright © 2016年 ucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
